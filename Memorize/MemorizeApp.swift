f15f519af166511c8d5f85d35b1887d900e5d28e
usanka <usanka@jrcsoftware.com>
2025-10-30 11:35:19 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Usanka Ranasinghe on 30/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
